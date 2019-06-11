58ae4d592cc575a12bd12d5e928a69029e8e0ec8
Allyn Alda <Teddy@Om.local>
2019-06-11 13:15:48 +0200

    Updates

---
+++ precache-manifest.799e7494e2d0ab5003a5cca66db95deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ea4dc2ab324130fa319ca82aa7b708",
+    "url": "/Digital-CV-Portfolio/index.html"
+  },
+  {
+    "revision": "95c1d9f4ca0b62b6a5f3",
+    "url": "/Digital-CV-Portfolio/static/css/main.bc473554.chunk.css"
+  },
+  {
+    "revision": "7eb940385fafa8d55183",
+    "url": "/Digital-CV-Portfolio/static/js/2.d94f9aef.chunk.js"
+  },
+  {
+    "revision": "95c1d9f4ca0b62b6a5f3",
+    "url": "/Digital-CV-Portfolio/static/js/main.321e8c17.chunk.js"
+  },
+  {
+    "revision": "83b8ffb334b1b4fb1627",
+    "url": "/Digital-CV-Portfolio/static/js/runtime~main.477229f1.js"
+  }
+]);
