8c149a5399a66616926446bc1e3caaa42f131bf0
Allyn Alda <Teddy@om.home>
2019-06-11 10:34:53 +0200

    first commit

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Hero } from "react-bulma-components/full";
+import { Heading } from "react-bulma-components/full";
 
 export default function Profile() {
     return (
         <div><section>
         <Hero color="light" size="medium">
         <Hero.Body>
-        <h1 className="title">
+        <Heading className="title is-1" renderAs="p">
         Allyn Alda
-        </h1>
-        <h2 className="subtitle">
+        </Heading>
+        <Heading subtitle renderAs="p">
         Fullstack Web Developer
-         </h2>
+         </Heading>
         <div>
             <a href="https://www.linkedin.com/in/allyn-alda/"><img className="icon-image" src="/images/linkedin.png" /></a>
             <a href="https://medium.com/@allyn_alda"><img className="icon-image" src="/images/medium.png" /></a>
