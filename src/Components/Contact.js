0cc4c0b0522aba74fe37620268b799b1dd75adf5
Allyn Alda <Teddy@om.home>
2019-06-12 16:27:46 +0200

    added CV

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Container } from 'react-bulma-components/full';
-import { Heading } from 'react-bulma-components/full';
+import { Heading, Button, Container } from 'react-bulma-components/full';
 
 export default function Contact() {
         return (
             <div>
                 <Container className="contact-space">
                     <p className="bd-notification is-success">
-                    <Heading size={5} renderAs="p">
-                    Feel free to contact me at:
-                    </Heading>
-                    <Heading subtitle renderAs="p">
-                    allyn.alda@gmail.com
-                    </Heading>
+                        <Heading size={5} renderAs="p">
+                        Feel free to contact me at:
+                        </Heading>
+                        <Heading subtitle renderAs="p">
+                        allyn.alda@gmail.com
+                        </Heading>
                     </p>
+                    <div className="button-space">
+                    <a href="./images/allyn-alda-cv.pdf" download><Button color="danger">
+                        Download CV
+                    </Button></a>
+                    </div>
                 </Container>
             </div>
         )
